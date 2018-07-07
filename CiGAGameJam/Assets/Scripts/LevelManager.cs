9e51c11d85d75a5e37435cdd62e2230d01192355
Yue <duyue36@gmail.com>
2018-07-07 11:42:32 +0800

    Scene Update, add a Proto_0 Scene.

---
+++ CiGAGameJam/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public CameraShake cameraShake;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E is pressed");
+            StartCoroutine(cameraShake.Shake(0.25f, 0.4f));
+        }
+	}
+}
